2dce969b36f2a4a0de8e35ca5223dbf6cafa08ed
Thomas Viel <thomas.viel@groupe-nehs.com>
2023-10-08 12:02:05 +0200

    refactor: correct existing unit test and complete test suite

--- tests/TemplateManagerTest.php
+++ tests/TemplateManagerTest.php
@@ -1,5 +1,6 @@
 <?php
 
+
 require_once __DIR__ . '/../src/Entity/Destination.php';
 require_once __DIR__ . '/../src/Entity/Quote.php';
 require_once __DIR__ . '/../src/Entity/Site.php';
@@ -13,66 +14,182 @@ require_once __DIR__ . '/../src/Repository/QuoteRepository.php';
 require_once __DIR__ . '/../src/Repository/SiteRepository.php';
 require_once __DIR__ . '/../src/TemplateManager.php';
 
-class TemplateManagerTest extends PHPUnit_Framework_TestCase
+class TemplateManagerTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * Init the mocks
-     */
-    public function setUp()
-    {
-    }
+    private \Faker\Generator $faker;
+    private TemplateManager $templateManager;
+    private Destination $expectedDestination;
+    private Site $expectedSite;
+    private User $currentUser;
 
-    /**
-     * Closes the mocks
-     */
-    public function tearDown()
+
+    protected function setUp(): void
     {
+        $this->faker = \Faker\Factory::create();
+        $this->templateManager = new TemplateManager();
+        $this->expectedDestination = DestinationRepository::getInstance()->getById($this->faker->randomNumber());
+        $this->expectedSite = SiteRepository::getInstance()->getById($this->faker->randomNumber());
+        $this->currentUser = ApplicationContext::getInstance()->getCurrentUser();
     }
 
     /**
      * @test
      */
-    public function test()
+    public function testDestinationName()
     {
-        $faker = \Faker\Factory::create();
-
-        $expectedDestination = DestinationRepository::getInstance()->getById($faker->randomNumber());
-        $expectedUser = ApplicationContext::getInstance()->getCurrentUser();
-
-        $quote = new Quote($faker->randomNumber(), $faker->randomNumber(), $faker->randomNumber(), $faker->date());
-
+        $quote = new Quote($this->faker->randomNumber(), $this->faker->randomNumber(), $this->expectedDestination->id, $this->faker->date());
         $template = new Template(
             1,
             'Votre voyage avec une agence locale [quote:destination_name]',
-            "
-Bonjour [user:first_name],
-
-Merci d'avoir contacté un agent local pour votre voyage [quote:destination_name].
-
-Bien cordialement,
-
-L'équipe Evaneos.com
-www.evaneos.com
-");
-        $templateManager = new TemplateManager();
-
-        $message = $templateManager->getTemplateComputed(
+            "Merci d'avoir contacté un agent local pour votre voyage [quote:destination_name]"
+        );
+        $message = $this->templateManager->getTemplateComputed(
             $template,
             [
                 'quote' => $quote
             ]
         );
 
-        $this->assertEquals('Votre voyage avec une agence locale ' . $expectedDestination->countryName, $message->subject);
-        $this->assertEquals("
-Bonjour " . $expectedUser->firstname . ",
+        $this->assertEquals('Votre voyage avec une agence locale ' . $this->expectedDestination->countryName, $message->subject);
+        $this->assertEquals("Merci d'avoir contacté un agent local pour votre voyage " . $this->expectedDestination->countryName, $message->content);
+    }
 
-Merci d'avoir contacté un agent local pour votre voyage " . $expectedDestination->countryName . ".
+    /**
+     * @test
+     */
+    public function testDestinationLink()
+    {
+        $quote = new Quote($this->faker->randomNumber(), $this->expectedSite->id, $this->expectedDestination->id, $this->faker->date());
 
-Bien cordialement,
+        $template = new Template(
+            1,
+            'Votre voyage avec une agence locale [quote:destination_name]',
+            'Vous pouvez accéder à votre voyage sur le site [quote:destination_link]'
+        );
+        $message = $this->templateManager->getTemplateComputed(
+            $template,
+            [
+                'quote' => $quote
+            ]
+        );
 
-L'équipe Evaneos.com
-www.evaneos.com
-", $message->content);
+        $this->assertEquals('Votre voyage avec une agence locale ' . $this->expectedDestination->countryName, $message->subject);
+        $this->assertEquals('Vous pouvez accéder à votre voyage sur le site ' . $this->expectedSite->url . '/' . $this->expectedDestination->countryName . '/quote/' . $quote->id . '', $message->content);
+    }
+
+    /**
+     * @test
+     */
+    public function testSummary()
+    {
+        $quote = new Quote($this->faker->randomNumber(), $this->faker->randomNumber(), $this->expectedDestination->id, $this->faker->date());
+
+        $template = new Template(
+            1,
+            'Summary subject : [quote:summary]',
+            'Summary content : [quote:summary]'
+        );
+        $message = $this->templateManager->getTemplateComputed(
+            $template,
+            [
+                'quote' => $quote
+            ]
+        );
+
+        $this->assertEquals('Summary subject : ' . $quote->id, $message->subject);
+        $this->assertEquals('Summary content : ' . $quote->id, $message->content);
+    }
+
+    /**
+     * @test
+     */
+    public function testSummaryHTML()
+    {
+        $quote = new Quote($this->faker->randomNumber(), $this->faker->randomNumber(), $this->expectedDestination->id, $this->faker->date());
+
+        $template = new Template(
+            1,
+            'Summary subject : [quote:summary_html]',
+            'Summary content : [quote:summary_html]'
+        );
+        $message = $this->templateManager->getTemplateComputed(
+            $template,
+            [
+                'quote' => $quote
+            ]
+        );
+
+        $this->assertEquals('Summary subject : <p>' . $quote->id . '</p>', $message->subject);
+        $this->assertEquals('Summary content : <p>' . $quote->id . '</p>', $message->content);
+    }
+
+    /**
+     * @test
+     */
+    public function testApplicationContextUser()
+    {
+        $quote = new Quote($this->faker->randomNumber(), $this->expectedSite->id, $this->expectedDestination->id, $this->faker->date());
+
+        $template = new Template(
+            1,
+            '[user:first_name], votre voyage est prêt',
+            'Bonjour [user:first_name]'
+        );
+        $message = $this->templateManager->getTemplateComputed(
+            $template,
+            [
+                'quote' => $quote
+            ]
+        );
+
+        $this->assertEquals($this->currentUser->firstname . ', votre voyage est prêt', $message->subject);
+        $this->assertEquals('Bonjour ' . $this->currentUser->firstname, $message->content);
+    }
+
+    /**
+     * @test
+     */
+    public function testSpecificUser()
+    {
+        $quote = new Quote($this->faker->randomNumber(), $this->expectedSite->id, $this->expectedDestination->id, $this->faker->date());
+        $user = new User($this->faker->randomNumber(), $this->faker->firstName, $this->faker->lastName, $this->faker->email);
+
+        $template = new Template(
+            1,
+            '[user:first_name], votre voyage est prêt',
+            'Bonjour [user:first_name]'
+        );
+        $message = $this->templateManager->getTemplateComputed(
+            $template,
+            [
+                'quote' => $quote,
+                'user' => $user
+            ]
+        );
+
+        $this->assertEquals($user->firstname . ', votre voyage est prêt', $message->subject);
+        $this->assertEquals('Bonjour ' . $user->firstname, $message->content);
+    }
+    /**
+     * @test
+     */
+    public function testWithoutQuoteData()
+    {
+        $user = new User($this->faker->randomNumber(), $this->faker->firstName, $this->faker->lastName, $this->faker->email);
+
+        $template = new Template(
+            1,
+            '[user:first_name], votre voyage est prêt',
+            'Bonjour [user:first_name]'
+        );
+        $message = $this->templateManager->getTemplateComputed(
+            $template,
+            [
+                'user' => $user
+            ]
+        );
+
+        $this->assertEquals($user->firstname . ', votre voyage est prêt', $message->subject);
+        $this->assertEquals('Bonjour ' . $user->firstname, $message->content);
     }
 }
